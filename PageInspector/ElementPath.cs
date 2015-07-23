c68598f8f7a181556cd7b10c8a722432e75d2b28
marriola <azathoth500@gmail.com>
2015-07-22 22:42:22 -0400

    Separated into own file. Added FromString method.

---
+++ PageInspector/ElementPath.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperDesigner
+{
+    public class ElementPath
+    {
+        public List<Selector> path;
+
+        public ElementPath()
+        {
+            path = new List<Selector>();
+        }
+
+        public ElementPath(ElementPath existingPath)
+        {
+            path = new List<Selector>(existingPath.path);
+        }
+
+        public static ElementPath FromString(string pathString)
+        {
+            ElementPath path = new ElementPath();
+            StringBuilder thisNode = new StringBuilder();
+            foreach (char c in pathString)
+            {
+                if (c == '/')
+                {
+                    path.path.Add(Selector.FromString(thisNode.ToString()));
+                    thisNode.Clear();
+                    continue;
+                }
+
+                pathString = pathString.Substring(1);
+                thisNode.Append(c);
+            }
+
+            if (thisNode.Length > 0)
+            {
+                path.path.Add(Selector.FromString(thisNode.ToString()));
+            }
+            return path;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Selector selector in path.Reverse<Selector>())
+            {
+                sb.Append(selector.ToString(false));
+                sb.Append('/');
+            }
+            return sb.ToString();
+        }
+    }
+}
