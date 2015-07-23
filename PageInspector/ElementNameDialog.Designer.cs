6ddcb60f510c67fed4a0777d98dfc834d4f5d93d
marriola <azathoth500@gmail.com>
2015-07-22 22:36:18 -0400

    Added outer/inner element select buttons.

--- PageInspector/ElementNameDialog.Designer.cs
+++ PageInspector/ElementNameDialog.Designer.cs
@@ -28,10 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ElementNameDialog));
             this.txtElementName = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.txtElementInfo = new System.Windows.Forms.TextBox();
+            this.chkOptional = new System.Windows.Forms.CheckBox();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtElementName
@@ -40,12 +44,14 @@
             this.txtElementName.Name = "txtElementName";
             this.txtElementName.Size = new System.Drawing.Size(225, 20);
             this.txtElementName.TabIndex = 0;
+            this.txtElementName.TextChanged += new System.EventHandler(this.txtElementName_TextChanged);
             // 
             // button1
             // 
             this.button1.AllowDrop = true;
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(12, 65);
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 89);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(49, 23);
             this.button1.TabIndex = 1;
@@ -55,7 +61,7 @@
             // button2
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(67, 65);
+            this.button2.Location = new System.Drawing.Point(67, 89);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -70,13 +76,46 @@
             this.txtElementInfo.Size = new System.Drawing.Size(225, 20);
             this.txtElementInfo.TabIndex = 3;
             // 
+            // chkOptional
+            // 
+            this.chkOptional.AutoSize = true;
+            this.chkOptional.Location = new System.Drawing.Point(12, 66);
+            this.chkOptional.Name = "chkOptional";
+            this.chkOptional.Size = new System.Drawing.Size(136, 17);
+            this.chkOptional.TabIndex = 4;
+            this.chkOptional.Text = "This element is optional";
+            this.chkOptional.UseVisualStyleBackColor = true;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Image = ((System.Drawing.Image)(resources.GetObject("btnUp.Image")));
+            this.btnUp.Location = new System.Drawing.Point(175, 86);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(28, 28);
+            this.btnUp.TabIndex = 5;
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Image = ((System.Drawing.Image)(resources.GetObject("btnDown.Image")));
+            this.btnDown.Location = new System.Drawing.Point(209, 86);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(28, 28);
+            this.btnDown.TabIndex = 6;
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
             // ElementNameDialog
             // 
             this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.button2;
-            this.ClientSize = new System.Drawing.Size(250, 99);
+            this.ClientSize = new System.Drawing.Size(250, 123);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.chkOptional);
             this.Controls.Add(this.txtElementInfo);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -96,5 +135,8 @@
         private System.Windows.Forms.Button button2;
         internal System.Windows.Forms.TextBox txtElementName;
         internal System.Windows.Forms.TextBox txtElementInfo;
+        internal System.Windows.Forms.CheckBox chkOptional;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
     }
 }
--- PageInspector/ElementNameDialog.cs
+++ PageInspector/ElementNameDialog.cs
@@ -12,9 +12,64 @@ namespace ScraperDesigner
 {
     public partial class ElementNameDialog : Form
     {
-        public ElementNameDialog()
+        private HtmlElement _element;
+        
+        public HtmlElement Element
+        {
+            private set
+            {
+                _element = value;
+            }
+
+            get
+            {
+                return _element;
+            }
+        }
+
+        private Stack<HtmlElement> skippedElements;
+
+        public ElementNameDialog(HtmlElement element)
         {
             InitializeComponent();
+            this._element = element;
+            skippedElements = new Stack<HtmlElement>();
+            UpdateButtons();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateButtons()
+        {
+            btnDown.Enabled = skippedElements.Count > 0;
+            btnUp.Enabled = _element != null && _element.Parent != null && _element.Parent.TagName != "BODY";
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+
+            txtElementInfo.Text = DesignerWindow.ElementInfoString(_element.Parent);
+            skippedElements.Push(_element);
+            _element = _element.Parent;
+            _element.Focus();
+            UpdateButtons();
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            _element = skippedElements.Pop();
+            txtElementInfo.Text = DesignerWindow.ElementInfoString(_element);
+            _element.Focus();
+            UpdateButtons();
+        }
+
+        private void txtElementName_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = txtElementName.Text.Length > 0;
+        }
+
     }
 }
