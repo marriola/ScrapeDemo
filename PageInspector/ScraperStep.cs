ac651a596e77d36406a79085e76276c7ef37b205
marriola <azathoth500@gmail.com>
2015-07-22 22:42:55 -0400

    Separated Selector, ElementPath and ElementDefinition into their own files.

--- PageInspector/ScraperStep.cs
+++ PageInspector/ScraperStep.cs
@@ -1,26 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ScraperDesigner
 {
-    public struct Selector
-    {
-        public string Tag;
-        public string Name;
-        public string ClientId;
-        public string ClassName;
-    }
-
-    public struct ElementDefinition
-    {
-        public System.Windows.Forms.HtmlElement Element;
-        public string Id;
-        public Selector ElementSelector;
-    }
-
     public class ScraperStep
     {
         public string Url;
