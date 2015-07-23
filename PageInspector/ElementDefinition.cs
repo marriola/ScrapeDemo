b91a8e5a2d98aeb5fefaee2873d2460b5e20416a
marriola <azathoth500@gmail.com>
2015-07-22 22:42:32 -0400

    Separated into own file.

---
+++ PageInspector/ElementDefinition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperDesigner
+{
+    public struct ElementDefinition
+    {
+        public System.Windows.Forms.HtmlElement Element;
+        public string Id;
+        public Selector ElementSelector;
+    }
+}
